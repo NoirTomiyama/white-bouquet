fde34b5e4dd02995c611b9da4bc5ca702b80ea77
Ryota Tomiyama <lit.karu.noir@gmail.com>
2020-10-16 14:35:03 +0000

    add: s3

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,25 @@
 class TopController < ApplicationController
   def index
     @message = "index"
+    s3 = Aws::S3::Resource.new(
+      region: "ap-northeast-1",
+      credentials: Aws::Credentials.new(
+        ENV['AWS_ACCESS_KEY_ID'],
+        ENV['AWS_SECRET_ACCESS_KEY']
+      )
+    )
+    bucket = s3.bucket('my-bucket-sample-1038')
+
+    @images = []
+    bucket.objects.each do |item|
+      puts "Name:  #{item.key}"
+      puts "URL:   #{item.presigned_url(:get)}"
+      # @image_url = item.presigned_url(:get)
+      @images.push(item.presigned_url(:get))
+    end
+    # obj = bucket.object('test/test.png')
+    # @image_url = obj.presigned_url(:get)
+    puts @images.size
   end
     
   def show
