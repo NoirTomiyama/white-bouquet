8697b151ad4854dcd4afcedb37ea620e193b5a58
Ryota Tomiyama <lit.karu.noir@gmail.com>
2020-10-17 15:07:49 +0000

    Add: hashの作成

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,24 +1,47 @@
 class TopController < ApplicationController
   def index
     @message = "index"
-    s3 = Aws::S3::Resource.new(
+
+    resource = Aws::S3::Resource.new(
       region: "ap-northeast-1",
       credentials: Aws::Credentials.new(
         ENV['AWS_ACCESS_KEY_ID'],
         ENV['AWS_SECRET_ACCESS_KEY']
       )
     )
-    bucket = s3.bucket('my-bucket-sample-1038')
+    bucket_name = 'my-bucket-sample-1038'
+    bucket = resource.bucket('my-bucket-sample-1038')
 
-    @images = []
-    bucket.objects.each do |item|
-      puts "Name:  #{item.key}"
-      puts "URL:   #{item.presigned_url(:get)}"
-      # @image_url = item.presigned_url(:get)
-      @images.push(item.presigned_url(:get))
+    client = Aws::S3::Client.new(
+      region: "ap-northeast-1",
+      credentials: Aws::Credentials.new(
+        ENV['AWS_ACCESS_KEY_ID'],
+        ENV['AWS_SECRET_ACCESS_KEY']
+      )
+    )
+
+    @object_list = []
+    bucket.objects.each do |object|
+      if((object.key).include?('result/')) then
+        data = JSON.parse(client.get_object(bucket: bucket_name, key: object.key).body.read)
+        image = bucket.object(data['image'])
+        image_url = image.presigned_url(:get)
+        name = data['name']
+        score = data['score']
+        item = {
+          image: image_url,
+          name: name,
+          score: score
+        }
+        @object_list.push(item)
+      end
     end
-    # obj = bucket.object('test/test.png')
-    # @image_url = obj.presigned_url(:get)
+
+    @object_list.each do |object|
+      puts object[:image]
+      puts object[:name]
+      puts object[:score].round
+    end
   end
     
   def show
